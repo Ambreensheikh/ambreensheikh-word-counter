3f561411465c87963f2041024ef0d472422fe981
Ambreen Sheikh <ambreensheikh900@gmail.com>
2024-04-17 01:10:13 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answers = await inquirer.prompt([{
+        name: "Sentence",
+        type: "input",
+        message: "Enter your Sentence to words count."
+    }]);
+const words = answers.Sentence.trim().split(" ");
+console.log(words);
+console.log(`Your Sentence word count is ${words.length}`);
